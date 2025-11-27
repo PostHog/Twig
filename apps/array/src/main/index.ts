9c1a58be012a11b4dfdcf137f03d5ed506e07e34
JonathanLab <jonathanmieloo@gmail.com>
2025-11-27 13:50:01 +0100

    fix: persist login by setting persist:main

--- apps/array/src/main/index.ts
+++ apps/array/src/main/index.ts
@@ -85,6 +85,7 @@ function createWindow(): void {
       contextIsolation: true,
       preload: path.join(__dirname, "preload.js"),
       enableBlinkFeatures: "GetDisplayMedia",
+      partition: "persist:main",
     },
   });
 
