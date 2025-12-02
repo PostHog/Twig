b87976974a91b0d96d7e28fbd18fd2f99c741a44
JonathanLab <jonathanmieloo@gmail.com>
2025-12-02 22:59:58 +0100

    codeql

--- apps/array/src/shared/utils/id.ts
+++ apps/array/src/shared/utils/id.ts
@@ -1,7 +1,9 @@
-import { randomBytes } from "node:crypto";
-
 export function randomSuffix(length = 8): string {
-  return randomBytes(length).toString("hex").substring(0, length);
+  const array = new Uint8Array(length);
+  crypto.getRandomValues(array);
+  return Array.from(array, (b) => b.toString(16).padStart(2, "0"))
+    .join("")
+    .substring(0, length);
 }
 
 export function generateId(prefix: string, length = 8): string {
