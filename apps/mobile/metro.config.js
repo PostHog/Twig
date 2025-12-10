43f5aa845c7a511b55731d9e5f02a403fbeaf06a
JonathanLab <jonathanmieloo@gmail.com>
2025-12-10 11:59:14 +0000

    prevent picking up electron react

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,6 +1,24 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, "../..");
+
+const config = getDefaultConfig(projectRoot);
+
+// Watch monorepo root for changes
+config.watchFolders = [monorepoRoot];
+
+// Let Metro find modules in both locations
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(monorepoRoot, "node_modules"),
+];
+
+// Force React to resolve from monorepo root
+config.resolver.extraNodeModules = {
+  react: path.resolve(monorepoRoot, "node_modules/react"),
+};
 
 module.exports = withNativeWind(config, { input: "./global.css" });
