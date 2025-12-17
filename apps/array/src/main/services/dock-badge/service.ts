6914a24202ebdb8c267880d21af51106bc30a754
Charles Vien <charles.v@posthog.com>
2025-12-16 19:43:19 -0800

    Update badge to use . instead of 1

--- apps/array/src/main/services/dock-badge/service.ts
+++ apps/array/src/main/services/dock-badge/service.ts
@@ -19,7 +19,7 @@ export class DockBadgeService {
 
     this.hasBadge = true;
     if (process.platform === "darwin" || process.platform === "linux") {
-      app.setBadgeCount(1);
+      app.dock.setBadge(".");
     }
     log.info("Dock badge shown");
   }
@@ -29,7 +29,7 @@ export class DockBadgeService {
 
     this.hasBadge = false;
     if (process.platform === "darwin" || process.platform === "linux") {
-      app.setBadgeCount(0);
+      app.dock.setBadge("");
     }
     log.info("Dock badge cleared");
   }
