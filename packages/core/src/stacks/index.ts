cee149d9fc552d34358dff1d86ce4a441b3242d0
Jonathan Mieloo <32547391+jonathanlab@users.noreply.github.com>
2026-01-09 16:17:12 +0100

    feat: add @array/core stack management (#434)

---
+++ packages/core/src/stacks/index.ts
@@ -0,0 +1,11 @@
+export { updateStackComments } from "./comments";
+export { getEnrichedLog } from "./enriched-log";
+export { getMergeStack, mergeStack } from "./merge";
+export {
+  cleanupMergedChange,
+  findMergedChanges,
+  type MergedChange,
+  type ReparentResult,
+  reparentAndCleanup,
+} from "./merged";
+export { submitStack } from "./submit";
