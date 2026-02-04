03ca4ca5a7c0481ecccd460475f3dddc731f0e52
Joshua Snyder <joshua@posthog.com>
2026-02-04 16:04:42 +0100

    resolved conflicts, i think?

--- apps/twig/src/renderer/features/task-detail/components/TaskInput.tsx
+++ apps/twig/src/renderer/features/task-detail/components/TaskInput.tsx
@@ -153,7 +153,9 @@ export function TaskInput() {
               value={workspaceMode}
               onChange={(mode) => {
                 setWorkspaceMode(mode);
-                setLastUsedLocalWorkspaceMode(mode);
+                if (mode !== "cloud") {
+                  setLastUsedLocalWorkspaceMode(mode);
+                }
               }}
               size="1"
             />
